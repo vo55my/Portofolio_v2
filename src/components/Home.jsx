2c9dd511236e5e16044b521681a9cf3368e71cb4
Dhaifullah HIlmy <dhaifullahhilmy@gmail.com>
2024-09-21 16:36:42 +0700

    Second Commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,10 +4,10 @@ const Home = () => {
   return (
     <div id="home" className="page">
       <div className="row">
-        <div className="col-lg-4 col-sm-6">
+        <div className="col-6 col-lg-4 col-sm-6">
           <img className="img-thumbnail" src={Profil} alt="Profil" />
         </div>
-        <div className="col-lg-8 col-sm-6 d-flex flex-column justify-content-center">
+        <div className="col-6 col-lg-8 col-sm-6 d-flex flex-column justify-content-center">
           <h1 className="fw-bold">Dhaifullah Hilmy</h1>
           <h2>Software Engineer</h2>
         </div>
